a4fbb7faf6310990219e68b74d5d1f2106a8d7cc
Clifford Richardson <cmrallen@gmail.com>
2018-07-02 02:06:53 +0200

    Minor work on hub node

--- hub/node.go
+++ hub/node.go
@@ -7,6 +7,7 @@ import (
 // SkataNode is the representation of a piece of the Skata network
 // that the program can recognize without confusion
 type SkataNode struct {
+	pipe
 	ID   common.SkataNodeID
 	Type common.SkataNodeType
 }
